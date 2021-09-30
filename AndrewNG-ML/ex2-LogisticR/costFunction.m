4ac5a32890633e86cd8d0c63f9f2cd510363e6cd
PeterWrighten <1352213575@qq.com>
2021-10-01 02:21:21 +0900

    update ex2
    a hard work

--- AndrewNG-ML/ex2-LogisticR/costFunction.m
+++ AndrewNG-ML/ex2-LogisticR/costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -19,6 +19,15 @@ grad = zeros(size(theta));
 %
 % Note: grad should have the same dimensions as theta
 %
+
+J = 1./m .* sum(-y .* log(sigmoid(X * theta)) - (1 - y) .* log(1 - sigmoid(X * theta)));
+
+
+grad = 1./m .* sum((sigmoid(X * theta) - y) .* X);
+
+
+
+
 
 
 
