275a17b9c19aee61c56500bf05473b56077e3ea9
PeterWrighten <peterwrighten@gmail.com>
2023-07-04 20:09:33 +0900

    fix bug

--- Intro/download.py
+++ Intro/download.py
@@ -24,7 +24,25 @@ def download_url(file_name, url):
 
             print(f"(file_url) downloaded successfully to {download_path}")
 
+def search_dup(file_name, directory):
+    path = os.getcwd()
+    files = os.listdir(path)
+    for file in files:
+        if file_name in file:
+            return True
+
+    return False
+
+inte = 0
 for num in range(1, 23):
-    file_name = f"cs188-fa22-note{num}.pdf"
-    download_url(file_name, "https://inst.eecs.berkeley.edu/~cs188/fa22/")
-print("You successfully downloaded all files!")
+    file_name = f"cs188-fa22-note{num}.pdf" if num >= 10 else f"cs188-fa22-note0{num}.pdf"
+    if not search_dup:
+        download_url(file_name, "https://inst.eecs.berkeley.edu/~cs188/fa22/")
+        inte += 1
+    else:
+        inte += 1
+
+
+assert(inte == 22)
+
+print("You successfully downloaded all notes!")
